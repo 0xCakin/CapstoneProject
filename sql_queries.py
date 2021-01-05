81f71bdc0e94a18e58db841d97ecc412859c6d38
canerakin111 <canerakin111@gmail.com>
2021-01-04 21:10:13 -0500

    Three indentation was missing

--- sql_queries.py
+++ sql_queries.py
@@ -5,16 +5,16 @@ temperature_table_drop = "DROP TABLE IF EXISTS temperature"
 
 # CREATE TABLES
 # Immigration Table Create Statement
-songplay_table_create = """
+immigration_table_create = """
     CREATE TABLE IF NOT EXISTS immigration (
-    					    cicid    FLOAT PRIMARY KEY,
+    					cicid    FLOAT PRIMARY KEY,
 					    year     FLOAT,
 					    month    FLOAT,
 					    city     FLOAT,
 					    res      FLOAT,
-					    i94port  VARCHAR(3),
+					    iport  VARCHAR,
 					    arrdate  FLOAT,
-				            depdate  FLOAT,
+				        depdate  FLOAT,
 					    visa     FLOAT,
 					    addr     VARCHAR,
 					    AverageTemperature FLOAT,
@@ -29,19 +29,19 @@ temperature_table_create = """
                                       City VARCHAR,
                                       Country VARCHAR,
                                       Latitude VARCHAR,
-                                      Longitude VARCHAR
-                                      i94port VARCHAR(3)); """
+                                      Longitude VARCHAR,
+                                      iport VARCHAR); """
 
 # INSERT RECORDS
 # Immigration Table Insert Statement
-immigration_table_insert = """ INSERT INTO immigration (cicid, year, month, city, res, i94port, arrdate, depdate, visa, addr, AverageTemperature, LATITUDE, LONGITUDE)
+immigration_table_insert = """ INSERT INTO immigration (cicid, year, month, city, res, iport, arrdate, depdate, visa, addr, AverageTemperature, LATITUDE, LONGITUDE)
                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
 # Temperature Table Insert Statement
-temperature_table_insert = """ INSERT INTO users (AverageTemperature, City, Country, Latitude, Longitude, i94port)
+temperature_table_insert = """ INSERT INTO users (AverageTemperature, City, Country, Latitude, Longitude, iport)
                         VALUES (%s, %s, %s, %s, %s, %s)
                         """
 
 
 # QUERY LISTS
-create_table_queries = [songplay_table_create, temperature_table_create]
+create_table_queries = [immigration_table_create, temperature_table_create]
 drop_table_queries = [immigration_table_drop, temperature_table_drop]
